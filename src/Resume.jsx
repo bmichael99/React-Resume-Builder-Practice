ad95effee59211f3ef26cf03d8db54a173d6d730
Michael <michaelbrandmeyer99@gmail.com>
2025-06-12 16:49:53 -0700

    Basic layout for resume

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,14 +1,35 @@
 import { useState } from 'react'
 import ResumeForm from './ResumeForm.jsx'
+import './Resume.css'
 
 function Resume({fullName,emailAddress,portfolioURL,githubURL}) {
 
   return (
     <div className="preview">
-      <h2>{fullName}</h2>
-      <p>Email: {emailAddress}</p>
-      <p>Portfolio: {portfolioURL}</p>
-      <p>GitHub: {githubURL}</p>
+      <div className="header">
+        <h2>{fullName}</h2>
+        <p>Email: {emailAddress}</p>
+        <p>Portfolio: {portfolioURL}</p>
+        <p>GitHub: {githubURL}</p>
+      </div>
+
+      <div className="education-section">
+
+      </div>
+      
+      <div className="skills-section">
+
+      </div>
+
+      <div className="experience-section">
+
+      </div>
+
+      <div className="project-section">
+
+      </div>
+
+      
     </div>
   );
 }
