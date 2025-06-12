9c5b79a1d6de88c33a46dcc4d5ca3c9e5adbd6bf
Michael <michaelbrandmeyer99@gmail.com>
2025-06-12 15:50:11 -0700

    Added inputs for name and contact info and their correlated functions, useStates, and props.

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,41 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+function FormInput({id, label, type = "text", value, onChange}){
+  return(
+    <div className="form-group">
+    <label htmlFor={id}>{label}</label>
+    <input type={type} value={value} id={id}
+    onChange={onChange}/>
+    </div>
+  );
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [fullName, setFullName] = useState("Your Name");
+  const [emailAddress, setEmailAddress] = useState("name@gmail.com");
+  const [portfolioURL, setPortfolioURL] = useState("portfolio.com");
+  const [githubURL, setGithubURL] = useState("github.com/name");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="form">
+
+      <FormInput id="nameInput" type = "text" value={fullName} label="First Name"
+       onChange={(event) => setFullName(event.target.value)}/>
+
+      <FormInput id="emailInput" type = "email" value={emailAddress} label="Email Address"
+       onChange={(event) => setEmailAddress(event.target.value)}/>
+
+      <FormInput id="portfolioInput" type = "url" value={portfolioURL} label="Portfolio URL"
+       onChange={(event) => setPortfolioURL(event.target.value)}/>
+
+       <FormInput id="githubInput" type = "url" value={githubURL} label="Github URL"
+       onChange={(event) => setGithubURL(event.target.value)}/>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
